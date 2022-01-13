bf27d937147f4f8d2a109294335cbcd3766d530c
Shahriyor <shahriyordeveloping@gmail.com>
2022-01-13 17:52:10 +0500

    initial

---
+++ app/src/scripts/index.ts
@@ -0,0 +1,26 @@
+function openDrop(el){
+    el.classList.toggle('active');
+}
+const headerButton: HTMLButtonElement =
+  document.querySelector(".header__button");
+const headerMenu: HTMLUListElement = document.querySelector(".header__menu");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+headerButton.onclick = menuToggle;
+window.onclick = (e: MouseEvent) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
+window.addEventListener('click', function(e :any){
+  if(e.target.classList.contains('header__link')){
+    menuToggle();
+  }
+})
