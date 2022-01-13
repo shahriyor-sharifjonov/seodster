f4024ccf1a5e37afd274fe8bca6f801ad9785af2
Shahriyor <shahriyordeveloping@gmail.com>
2022-01-13 22:44:14 +0500

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -45,10 +45,53 @@ var swiper = new Swiper(".diploms__slider", {
     768: {
       slidesPerView: 3,
       spaceBetween: 30,
-    }
+    },
     992: {
       slidesPerView: 4,
       spaceBetween: 30,
     },
   },
+});
+var swiper = new Swiper(".reviews__slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    }
+  },
+});
+$(document).ready(function() {
+  $(".set > a").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > a").removeClass("active");
+      $(this).addClass("active");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
+    }
+  });
 });
